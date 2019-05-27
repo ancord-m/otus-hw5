6ecb41c448b05a92c664a6c1ff0e6ab3f2b3a91e
Michael Latushko <ancord.m@yandex.ru>
2019-05-27 16:08:56 +0300

    Первый комит

---
+++ include/matrix.h
@@ -0,0 +1,5 @@
+template <typename T, int DefaultValue>
+class Matrix
+{
+public:
+};
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "matrix.h"
+
+int main(int argc, char *argv[])
+{
+	Matrix<int, -1> matrix;
+
+	return 0;
+}
