4d5494e834234eb5ed42c554f3d2751c0f71f7a2
Michael Latushko <ancord.m@yandex.ru>
2019-06-06 21:47:42 +0300

    Добавлена примитивная индексация массива

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -5,7 +5,26 @@ class Matrix
 {
 	Cell<T> c;
 
+	int m[3][3];
+
 public:
+
+	Matrix()
+	{
+		for(int i = 0; i < 3; i++)
+		{
+			for(int j = 0; j < 3; j++)
+			{
+				m[i][j] = i + j;
+			}
+		}
+	}
+
+	const int * operator[](int index_x)
+	{
+		return m[index_x];
+	}
+
 	int size(void)
 	{
 		return 0;
--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,9 @@ int main(int argc, char *argv[])
 {
 	Matrix<int, -1> matrix;
 
+	auto value = matrix[1][2];
+
+	std::cout << value << std::endl;
+
 	return 0;
 }
